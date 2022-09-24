02c0a7985a808d4962d52a5372f347874a832f8c
Abhishek <er.abhi9170@gmail.com>
2022-09-24 07:57:19 +0530

    assignment-09

---
+++ 09.05.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a number:"))
+i=2*n-1
+while i>=1:
+     print(i)
+     i-=2        
---
+++ 09.07.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a number:"))
+i=2*n
+while i>=1:
+     print(i)
+     i-=2        
